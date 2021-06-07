8c6b2d3036ae823c94accedddab3717954e454bd
zakariaAlhomsy <60083281+zakariaAlhomsy@users.noreply.github.com>
2021-06-06 22:40:49 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lab 10
+https://cse110-lab10-zakariaalhomsy.canny.io/
+
+
+The second page is supposed to remove all the content from the page or keep it
---
+++ scripts/script.js
@@ -0,0 +1,70 @@
+// script.js
+
+import { router } from './router.js';
+
+const headerText = document.querySelector('header > h1');
+const settings = document.querySelector('header > img');
+
+// When the back button is hit, set the state with the new page
+window.addEventListener('popstate', e => {
+  if (e.state?.page && e.state.page.startsWith('entry')) {
+    router.setState('entry', true, Number(e.state.page.substr(5, e.state.page.length)));
+  } else {
+    router.setState(e.state?.page, true);
+  }
+});
+
+// Go to header page when header button is clicked
+headerText.addEventListener('click', () => {
+  router.setState('home', false);
+});
+
+// Go to settings page when settings button is clicked
+settings.addEventListener('click', () => {
+  router.setState('settings', false);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://cse110lab6.herokuapp.com/entries')
+    .then(response => response.json())
+    .then(entries => {
+      entries.forEach(entry => {
+        let newPost = document.createElement('journal-entry');
+        newPost.entry = entry;
+        newPost.addEventListener('click', () => {
+          let numEntry = Array.from(document.querySelector('main').childNodes).indexOf(newPost);
+          router.setState('entry', false, numEntry + 1);
+        });
+        document.querySelector('main').appendChild(newPost);
+      });
+    });
+});
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+// Instantiate the SDK. CDN will expose splitio globally 
+var factory = splitio({ 
+  core: {
+    authorizationKey: 'ud0lbd78uputbn440s0valv0n5c8rum414nk',
+    
+    // your internal user id, or the account id that 
+    // the user belongs to. 
+    // This coudld also be a cookie you generate  18h9r5qrmrqja1bsgrnms5icbj3hrnvomocp
+    // for anonymous users
+    key: 'user' + getRandomInt(100000000000),
+    // an OPTIONAL traffic type, if provided will be
+    // used for event tracking with the SDK client.
+    //trafficType: 'A_TRAFFIC_TYPE'
+  }
+});
+// And get the client instance you'll use
+var client = factory.client();
+
+client.on(client.Event.SDK_READY, function() {
+  var treatment = client.getTreatment("column");
+  if (treatment == "on") {
+    document.querySelector('main').classList.add('double-column');
+  } else if (treatment == "off") {
+    document.querySelector('main').classList.remove('double-column');
+  }
+});
